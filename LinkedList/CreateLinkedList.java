78f9a784e52cd82eee5adc21d5fb8bd2dfe04d63
IMPERIALLEGEND <tejaswawadekar@gmail.com>
2024-04-04 11:04:32 +0530

    Added String and linkedlist codes

--- LinkedList/CreateLinkedList.java
+++ LinkedList/CreateLinkedList.java
@@ -14,7 +14,10 @@ public class CreateLinkedList {
         head.next.next = new Node(30);
         head.next.next.next = new Node(40);
 
+        head = insertNode(head, 5, 0);
+        head = insertNode(head, 50, -1);
         print(head);
+        //recursive_print(head);
     }
 
     public static void print(Node head){
@@ -23,5 +26,44 @@ public class CreateLinkedList {
             System.out.print(temp.data + " ");
             temp = temp.next;
         }
+    }
+
+    public static void recursive_print(Node temp){
+        if(temp == null){
+            return;
+        }
+        System.out.print(temp.data + " ");
+        recursive_print(temp.next);
+    }
+
+    public static Node insertNode(Node head, int data, int pos){
+        if(pos == 0){
+            return insertNodeAtHead(head, data);
+        }
+
+        if(pos == -1){
+            return insertNodeAtEnd(head, data);
+        }
+
+        return null;
+    }
+
+    public static Node insertNodeAtHead(Node head, int data){
+        Node temp = new Node(data);
+        temp.next = head;
+        return temp;
+    }
+
+    public static Node insertNodeAtEnd(Node head, int data){
+        Node temp = new Node(data);
+        Node curr = head;
+
+        while(curr.next != null){
+            curr = curr.next;
+        }
+
+        curr.next = temp;
+
+        return head;
     }
 }
